72492a937f85201f219f3781dc903ec61c3f57a9
zhaozhihong <346537985@qq.com>
2020-04-04 16:45:30 +0800

    chore: （十三）：使用 Shiro 实现用户信息加密与登录认证

--- src/main/java/com/how2java/springboot/controller/LoginController.java
+++ src/main/java/com/how2java/springboot/controller/LoginController.java
@@ -2,7 +2,14 @@ package com.how2java.springboot.controller;
 
 import com.how2java.springboot.pojo.User;
 import com.how2java.springboot.result.Result;
+import com.how2java.springboot.result.ResultFactory;
 import com.how2java.springboot.service.UserService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.crypto.SecureRandomNumberGenerator;
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.HtmlUtils;
@@ -17,17 +24,56 @@ public class LoginController {
 
     @CrossOrigin
     @PostMapping(value = "api/login")
-    public Result login(@RequestBody User requestUser, HttpSession session) {
-        // 对 html 标签进行转义，防止 XSS 攻击
-        String username = HtmlUtils.htmlEscape(requestUser.getUsername());
-
-        User user = userService.get(username,requestUser.getPassword());
-        if(null==user) {
-            return new Result(400);
-        }else{
-            session.setAttribute("user",user);
-            return new Result(200);
+//    public Result login(@RequestBody User requestUser, HttpSession session) {
+//        // 对 html 标签进行转义，防止 XSS 攻击
+//        String username = HtmlUtils.htmlEscape(requestUser.getUsername());
+//
+//        User user = userService.get(username, requestUser.getPassword());
+//        if (null == user) {
+//            return new Result(400);
+//        } else {
+//            session.setAttribute("user", user);
+//            return new Result(200);
+//        }
+//    }
+    public Result login(@RequestBody User requestUser){
+        String username = requestUser.getUsername();
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(username, requestUser.getPassword());
+        try {
+            subject.login(usernamePasswordToken);
+            return ResultFactory.buildSuccessResult(username);
+        }catch (AuthenticationException e){
+            String message = "账号密码错误";
+            return ResultFactory.buildFailResult(message);
         }
+    }
+
+    @PostMapping("api/register")
+    @ResponseBody
+    public Result register(@RequestBody User user) {
+        String username = user.getUsername();
+        String password = user.getPassword();
+        username = HtmlUtils.htmlEscape(username);
+        user.setUsername(username);
+
+        boolean exist = userService.isExist(username);
+        if (exist) {
+            String message = "用户名已被使用";
+            return ResultFactory.buildFailResult(message);
+        }
+        // 生成盐，默认长度16位
+        String salt = new SecureRandomNumberGenerator().nextBytes().toString();
+        // 设置 hash 算法迭代次数
+        int times = 2;
+        // 得到hash 后的密码
+        String encodedPassword = new SimpleHash("md5", password, salt, times).toString();
+        // 存储用户信息，包括salt 与hash 后的密码
+        user.setSalt(salt);
+        user.setPassword(encodedPassword);
+        userService.add(user);
+
+        return ResultFactory.buildSuccessResult(user);
     }
 
 }
--- src/main/java/com/how2java/springboot/result/Result.java
+++ src/main/java/com/how2java/springboot/result/Result.java
@@ -1,18 +1,22 @@
 package com.how2java.springboot.result;
 
+import lombok.Data;
+
+@Data
 public class Result {
 
     private int code;
+    private String message;
+    private Object result;
 
     public Result(int code) {
         this.code = code;
     }
 
-    public int getCode() {
-        return code;
+    public Result(int code,String message, Object data) {
+        this.code = code;
+        this.message = message;
+        this.result = data;
     }
 
-    public void setCode(int code) {
-        this.code = code;
-    }
 }
